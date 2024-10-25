261c59161e791413bc87f3de57dbb6db200d2a07
gio299 <105156704+gio299@users.noreply.github.com>
2024-10-25 22:24:00 +0200

    Update kontra.py

--- core/platforms/kontra.py
+++ core/platforms/kontra.py
@@ -12,7 +12,7 @@ except:
     from bs4 import BeautifulSoup
 
 # URL of the webpage containing the wmsAuthSign
-webpage_url = 'https://kontrachannel.gr/livetv-kontrachannel'
+webpage_url = 'https://kontralive.siliconweb.com/live/kontratv/playlist.m3u8'
 
 # Fetch the content of the webpage
 response = requests.get(webpage_url)
